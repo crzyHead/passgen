c5033a27c7842afed44a982e94518105bcfeda27
Mykhailo Lisovskyi <37449089+crzyHead@users.noreply.github.com>
2022-06-30 23:48:37 +0300

    Update checker.py

--- checker.py
+++ checker.py
@@ -1,10 +1,10 @@
 import re
 
 def CheckPassword(pass_):
-    totalScore = uniqueness(pass_) + length(pass_) + diversity(pass_)
+    totalScore = uniqueness(pass_) + length(pass_) + diversity(pass_) # total score of password, where 100 is the best possible and 30 is the worst possible
     return totalScore
 
-def uniqueness(pass_):
+def uniqueness(pass_): # Check uniqueness of the password string, score 40 - no repeating symbols; score 15 - more than 50% of the password is the same symbol
     temp = ''.join(set(pass_))
     score = 40
     for i in range(10,5,-1):
@@ -12,13 +12,13 @@ def uniqueness(pass_):
         else: score -= 5
     return score
 
-def length(pass_):
+def length(pass_): # Check the length of the password string, score 35 - length of 20; score 15 - length of 8
     score = 35
     for i in [20, 12, 8]:
         if len(pass_) == i: return score
         else: score -= 10
 
-def diversity(pass_):
+def diversity(pass_): # Check diversity of the password string, score 24 - maximum diversity, score 0 - no diversity
     [u_, l_, n_, s_] = [len(re.sub(i,"",pass_)) for i in ["[^A-Z]", "[^a-z]", "[^0-9]", "[^\W]"]]
     ref = len(pass_)
     score = 0

b36c939c03fe1946c6c84d453550cde3ea18b27a
Mykhailo Lisovskyi <37449089+crzyHead@users.noreply.github.com>
2022-06-30 23:55:13 +0300

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,12 +1,12 @@
 from time import time
 
-def lcg(x, a, c, m):
+def lcg(x, a, c, m): # Linear Congrugential Generator, x - seed
     while True:
         x = (a * x + c) % m
         yield x
 
 
-def random_uniform_sample(n, interval, seed=0):
+def random_uniform_sample(n, interval, seed=0): # random uniform sample, randomness is achieved with seed
     a, c, m = 1103515245, 12345, 2 ** 31
     bsdrand = lcg(seed, a, c, m)
 
@@ -19,12 +19,12 @@ def random_uniform_sample(n, interval, seed=0):
 
     return sample
 
-def Generate(let_u=True, let_l=True, sym=True, num=True, length=20):
-    seed = int(time() * 1000)
-    l_up = {let_u:"ABCDEFGHIJKLMNOPQRSTUVWXYZ"}
-    l_low = {let_l:"abcdefghijklmnopqrstuvwxyz"}
-    _num = {num:"0123456789"}
-    _sym = {sym:"~!@#$%^*-_=+[{]}/;:,.?"}
+def Generate(let_u=True, let_l=True, sym=True, num=True, length=20): # Generator function, generates massive string based on inputs
+    seed = int(time() * 1000) # seed for the randomness, uses current milliseconds to epoch
+    l_up = {let_u:"ABCDEFGHIJKLMNOPQRSTUVWXYZ"} # if let_u is true, it will be added to general string
+    l_low = {let_l:"abcdefghijklmnopqrstuvwxyz"} # always true, no matter what
+    _num = {num:"0123456789"} # if num is true, numbers will be added to general string
+    _sym = {sym:"~!@#$%^*-_=+[{]}/;:,.?"} # if sym is true, symbols will be added to general string
     joined = ""
     for i in [l_up,l_low,_num,_sym]:
         if True in i: joined += i.get(True)
